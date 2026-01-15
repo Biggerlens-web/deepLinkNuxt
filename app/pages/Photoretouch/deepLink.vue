f771ffa9726ee4580e91296604672db14235f3a3
13005407321 <851209163@qq.com>
2026-01-15 17:35:48 +0800

    feat(路由): 在多个页面添加路由查询参数处理
    添加了路由查询参数处理逻辑，用于获取当前路由的查询参数

--- app/pages/Photoretouch/deepLink.vue
+++ app/pages/Photoretouch/deepLink.vue
@@ -7,6 +7,9 @@
 <script lang="ts" setup>
     import { onMounted } from 'vue';
     let timeout: any = null
+    const route = useRoute()
+    const hrefQuery = route.fullPath.split('?')[1]
+
     const appStoreUrl = `https://apps.apple.com/cn/app/photo-retouch-%E6%B6%88%E9%99%A4%E7%AC%94%E5%8F%8Ap%E5%9B%BE%E4%BF%AE%E5%9B%BE%E8%BD%AF%E4%BB%B6-%E7%A5%9E%E5%A5%87%E6%A9%A1%E7%9A%AE%E6%93%A6/id1230394683?platform=iphone`;
     const isIOS = typeof navigator !== 'undefined' && /iP(hone|od|ad)/.test(navigator.userAgent)
     const iosDeepUrl = `itms-apps://itunes.apple.com/app/id1230394683`
--- app/pages/knockout/function.vue
+++ app/pages/knockout/function.vue
@@ -9,6 +9,8 @@
     import logo from '@/assets/moblieLogo.png'
     import { onMounted } from 'vue';
     let timeout: any = null
+    const route = useRoute()
+    const hrefQuery = route.fullPath.split('?')[1]
     const appStoreLink = `https://apps.apple.com/cn/app/proknockout%E6%8A%A0%E5%9B%BEp%E5%9B%BE%E4%BF%AE%E5%9B%BE%E7%A5%9E%E5%99%A8-%E4%B8%93%E4%B8%9A%E6%99%BA%E8%83%BD%E8%AF%81%E4%BB%B6%E7%85%A7%E5%88%B6%E4%BD%9C/id944665061?platform=iphone`;
 
     onMounted(() => {
